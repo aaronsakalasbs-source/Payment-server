769e3a2006596a1a4fda4eb05ee5adf13ea15f5a
aaronsakalasbs-source <aaronsakalasbs@gmail.com>
2026-05-06 09:03:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
-app.use(cors()); // Accept all frontend originss
+app.use(cors()); // Accept all frontend origins
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
